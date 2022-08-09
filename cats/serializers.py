4b408516390adf553a811d5070583cff96b6a7cc
Vlad Shirokov <shirocov.vladislav2015@yandex.ru>
2022-08-09 22:11:54 +0300

    v

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,22 +1,44 @@
+import datetime as dt
+import webcolors
+
 from rest_framework import serializers
 
 from .models import Cat, Owner, Achievement
 
 
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(read_only=True)
+    # owner = serializers.StringRelatedField(read_only=True)
     achievement = AchievementSerializer(many=True, read_only=True)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()
 
     class Meta:
         model = Cat
-        fields = ('name', 'color', 'birth_year', 'owner', 'achievement')
+        fields = ('name', 'color', 'birth_year', 'owner', 'achievement', 'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
 
 class OwnerSerializer(serializers.ModelSerializer):
--- cats/views.py
+++ cats/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 from .models import Cat, Owner
 from .serializers import CatSerializer, OwnerSerializer
@@ -12,6 +13,12 @@ from .serializers import CatSerializer, OwnerSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
